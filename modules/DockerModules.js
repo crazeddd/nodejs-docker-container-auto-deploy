b4653a7b99ad937d3b5d6e7a13a0dab87f2450a7
Crazed <73705334+crazeddd@users.noreply.github.com>
2024-02-23 17:59:32 -0600

    Update DockerModules.js
    Updated container display

--- modules/DockerModules.js
+++ modules/DockerModules.js
@@ -93,23 +93,24 @@ async function displayContainers() {
 
         for (let container of containers) {
             const html = '\n' + `.container
-            .container-items
-              .name ${container.names}
-              button.containerReq.stop(id="${container.id}",
-                onclick="containerReq(this.classList[1], this.id)"
-              ) Stop
-              button.containerReq.start(id="${container.id}",
-                onclick="containerReq(this.classList[1], this.id)"
-              ) Start
-            .container-items.image
-              p#res ${container.image}
-            .container-items.directory
-              p#res ${container.dir}
-            .container-items
-              p#res RAM: 78%
-              p CPU: 56%
-            .container-items
-              .status.${container.status}`
+        .container-items.name
+          p.name-cont ${container.names}
+          button.containerReq.stop(id="${container.id}",
+            onclick="containerReq(this.classList[1], this.id)"
+          )
+            svg.run(xmlns="http://www.w3.org/2000/svg", viewBox="0 0 384 512")
+              path(
+                d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"
+              )
+        .container-items.image
+          p ${container.image}
+        .container-items.directory
+          p ${container.dir}
+        .container-items.usage
+          p RAM: 15%
+          p CPU: 23%
+        .container-items
+          .status.${container.status}`
 
             fs.appendFileSync('modules/html-modules/containers.pug', html, function (err) {
                 if (err) {
